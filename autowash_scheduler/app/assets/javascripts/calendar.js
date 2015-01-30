347ab35c8b0fd957655311989ef2c89ec51c7412
Nurbrun <bruno.alfano.rizzo@gmail.com>
2015-01-30 18:34:04 -0500

    added 3 dummy events, resized calendar div via css selector

--- autowash_scheduler/app/assets/javascripts/calendar.js
+++ autowash_scheduler/app/assets/javascripts/calendar.js
@@ -6,9 +6,25 @@ $(document).ready(function() {
         left : 'prev,next today',
         center : 'title',
         right : 'month,basicWeek,basicDay'
-    	}   
+    	},
+    	events: [
+        {
+            title  : 'event1',
+            start  : '2015-01-01'
+        },
+        {
+            title  : 'event2',
+            start  : '2015-01-05',
+            end    : '2015-01-07'
+        },
+        {
+            title  : 'event3',
+            start  : '2015-01-09T12:30:00',
+            allDay : false // will make the time show
+        }
+    ]   
 
-    })
+    });
   
 
 });
